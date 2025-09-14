50295a5964530e067bc953e39b6b35854203720c
Tori Rodgers <torirodgers22@gmail.com>
2025-09-13 19:32:56 -0500

    adjusted variables in layout to tailwind v4 standard. finished colors and glow for hero section

--- src/app/page.js
+++ src/app/page.js
@@ -2,25 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-			<div>
+		<div className="container mx-auto p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<div className="relative">
+				<div className="opacity-10">
+					<div className="absolute top-0 right-0 sm:w-[600px] sm:h-[600px] w-[300px] h-[300px] bg-purple-700/50 rounded-full blur-3xl"></div>
+					<div className="absolute top-4 right-4 sm:w-[400px] sm:h-[400px] w-[150px] h-[150px] bg-purple-500/60 rounded-full blur-2xl"></div>
+					<div className="absolute top-8 right-8 sm:w-[300px] sm:h-[300px] w-[100px] h-[100px] bg-purple-400/70 rounded-full blur-xl"></div>
+				</div>
 				<h1 className="text-4xl font-bold tracking-light">
-					Tori Rodgers <span className="block text-purple-700">Software Developer</span>
+					Tori Rodgers{" "}
+					<span className="block text-purple-700">Software Developer</span>
 				</h1>
-				<p className="mt-6 text-xl text-gray-700 leading-8">
+				<p className="mt-6 text-xl text-slate-300 leading-8">
 					Curious, driven, and just getting started. A developer focused on
 					building thoughtful, functional applications with clean code and
 					scalable structure. Currently deepening my skills in full-stack
 					development while exploring the intersection of software and design.
 				</p>
-        <div>
-          <button className="px-8 py-3 rounded-lg bg-purple-800 text-white font-medium hover:bg-purple-500">
-            About Me
-            </button>
-          <button className="px-8 py-3 rounded-lg border border-gray-600 hover:border-purple-500 font-medium">
-            Contact Me
-            </button>
-        </div>
+				<div className="flex mt-10 gap-4">
+					<button className="px-8 py-3 rounded-lg font-medium bg-purple-700 hover:bg-purple-500">
+						About Me
+					</button>
+					<button className="px-8 py-3 rounded-lg font-medium border border-slate-600 hover:border-purple-500">
+						Contact Me
+					</button>
+				</div>
 			</div>
 		</div>
 	);
